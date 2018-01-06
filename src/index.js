558eec44ed15f82199e3b4e6ba23033b6629812a
bison1994 <942915554@qq.com>
2018-01-06 23:30:18 +0800

    add image compress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './assets/demo.css'
 import './assets/main.css'
 
-import Myimg from './assets/gift.jpg'
+import Myimg from './assets/demo-2.png'
+
+var shuffle = require('lodash/shuffle')
 
 var img = new Image();
 img.src = Myimg;
 document.body.appendChild(img)
+
+var arr = shuffle([1, 2, 3])
+console.log(arr)
